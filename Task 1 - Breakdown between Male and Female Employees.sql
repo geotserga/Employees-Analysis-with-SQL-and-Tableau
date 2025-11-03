5ecf3bea3ca65377386a74773782adaa68b5eac0
Georgia Tserga <georgtser@gmail.com>
2025-11-03 23:53:33 +0200

    Add files via upload

---
+++ Task 1 - Breakdown between Male and Female Employees.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT
+    emp_no, from_date, to_date
+FROM 
+    t_dept_emp;
+
+SELECT 
+	YEAR(d.from_date) as calendar_year,
+    e.gender,
+    COUNT(e.emp_no) AS num_of_employees
+FROM 
+    t_employees e
+JOIN 
+    t_dept_emp d ON d.emp_no = e.emp_no
+GROUP BY calendar_year, e.gender
+HAVING calendar_year >= 1990;
